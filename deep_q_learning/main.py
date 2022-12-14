6e7950f967607a30b46c18c1fe24fd745630d418
Maria-Anson <mariaansona@gmail.com>
2022-12-14 11:18:05 -0500

    Deep q learning changes

--- deep_q_learning/main.py
+++ deep_q_learning/main.py
@@ -7,8 +7,9 @@ from pathlib import Path
 from collections import deque
 import random, datetime, os, copy
 
-from deep_q_learning.preprocess import SkipFrame, ResizeObservation, GrayScaleObservation
-from deep_q_learning.mario_agent import Mario
+from preprocess import SkipFrame, ResizeObservation, GrayScaleObservation
+from mario_agent import Mario
+from logger import MetricLogger
 
 # Gym is an OpenAI toolkit for RL
 import gym
@@ -21,7 +22,7 @@ from nes_py.wrappers import JoypadSpace
 # Super Mario environment for OpenAI Gym
 import gym_super_mario_bros
 
-env = gym_super_mario_bros.make("SuperMarioBros-1-1-v0", render_mode='rgb', apply_api_compatibility=True)
+env = gym_super_mario_bros.make("SuperMarioBros-1-1-v0", render_mode='rgb_array', apply_api_compatibility=True)
 env = JoypadSpace(env, [["right"], ["right", "A"]])
 env.reset()
 
@@ -40,3 +41,43 @@ print(f"Using CUDA: {use_cuda}")
 save_dir = Path("deep_q_learning\checkpoints") / datetime.datetime.now().strftime("%Y-%m-%dT%H-%M-%S")
 save_dir.mkdir(parents=True)
 
+mario = Mario(state_dim=(4, 84, 84), action_dim=env.action_space.n, save_dir=save_dir)
+
+logger = MetricLogger(save_dir)
+
+episodes = 10
+for e in range(episodes):
+
+    state = env.reset()
+
+    # Play the game!
+    while True:
+
+        env.render()
+
+        # Run agent on the state
+        action = mario.act(state)
+
+        # Agent performs action
+        next_state, reward, done, trunc, info = env.step(action)
+
+        # Remember
+        mario.cache(state, next_state, action, reward, done)
+
+        # Learn
+        q, loss = mario.learn()
+
+        # Logging
+        logger.log_step(reward, loss, q)
+
+        # Update state
+        state = next_state
+
+        # Check if end of game
+        if done or info["flag_get"]:
+            break
+
+    logger.log_episode()
+
+    if e % 20 == 0:
+        logger.record(episode=e, epsilon=mario.exploration_rate, step=mario.curr_step)
